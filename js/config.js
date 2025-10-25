f097ad312e860f4bdf6bbd34eaeb4f40f97e3e68
ready4exam <ready4urexam@gmail.com>
2025-10-25 17:24:11 +0530

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,2 @@
+window.SUPABASE_URL = 'https://gkyvojcmqsgdynmitcuf.supabase.co'; // Example: 'https://xyz123.supabase.co'
+window.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdreXZvamNtcXNnZHlubWl0Y3VmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA3NDQ0OTcsImV4cCI6MjA3NjMyMDQ5N30.5dn5HbXxQ5sYNECS9o3VxVeyL6I6Z2Yf-nmPwztx1hE'
